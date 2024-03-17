08577e9841dcb1ec68bc8aa8a50cf031878f8507
Sanidhya Khandelwal <sanidhiyakhandelwal450@gmail.com>
2024-03-17 19:56:37 +0530

    02_passing data from parent to child component using INPUT decorator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = '01AgIntro';
+  parentMessage: string = 'Message coming from parent component' //step 1 creating variable in parent that needs to be passed to child component using @input decorator 
+
 }
--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-post',
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class PostComponent {
   title: string = "List of posts";
+
+  //step 3 In your child component, define an Input property to receive the string data from the parent.
+  @Input()
+  fromParent!: string; //! is imp else we get error... X [ERROR] TS2564: Property 'fromParent' has no initializer and is not definitely assigned in the constructor. [plugin angular-compiler]
 }
