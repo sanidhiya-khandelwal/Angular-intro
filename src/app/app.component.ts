9e2206fb6be29e4ff97440217c38d00c7daf76e3
Sanidhya Khandelwal <sanidhiyakhandelwal450@gmail.com>
2024-03-20 19:23:49 +0530

    Passing data from Child component to Parent component Using @ViewChild decorator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
+import { PostComponent } from './post/post.component'; //step2 view ki..import compoenent
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit { //step 5 view ki.. implement interface
   title = '01AgIntro';
-  parentMessage: string = 'Message coming from parent component' //step 1 creating variable in parent that needs to be passed to child component using @input decorator 
+  parentMessage: string = 'Message coming from parent component'; //step 1 creating variable in parent that needs to be passed to child component using @input decorator 
+  displayChildMssg: string | undefined; //step3view ki.. creating variable
+
+  //ViewChild ki help se Child k saare variables/data accessible h
+  @ViewChild(PostComponent) childComp: any; //step1 view ki.. view child takes parent component and name of variable u can give any name
+
+  constructor() { //step6 view ki.. to  understabd th need of ngAfterViewInit()
+    console.log(this.childComp); // yh dega undefined "constructor run jb ho rha tha tb childComp m kuch nhi tha it was not intialized so undefined print ho rha tha"
+  }
+  ngAfterViewInit() { //step7 view ki.. lifecycle hook -> coz in constructor run jb ho rha tha tb childComp m kuch nhi tha it was not intialized so undefined print ho rha tha so we used this hook coming from interface "AfterViewInit" yh last m run hota yh tb call hota h jb pura wave initlaized hojaata h
+    console.log(this.childComp); // display in console
+    this.displayChildMssg = this.childComp.childMessage //step8 store the variable and do string interpolation
+  }
 
 }
--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -9,6 +9,8 @@ export class PostComponent {
   title: string = "List of posts";
   postParentMessage: string = 'Message coming from the post parent' //step1 creating variable
 
+  childMessage: string = 'From Child Component' //step1 view ki creating childmessage to be passed
+
   //step 3 In your child component, define an Input property to receive the string data from the parent.
   @Input()
   fromParent!: string; //! is imp else we get error... X [ERROR] TS2564: Property 'fromParent' has no initializer and is not definitely assigned in the constructor. [plugin angular-compiler]
